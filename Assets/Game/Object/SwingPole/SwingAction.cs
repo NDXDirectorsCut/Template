2b3a2b1e94e7f21041ec5099c0bd643d6e643ea4
NDXDirectorsCut <Rares_sincu@yahoo.com>
2025-05-28 07:36:22 +0300

    append2

--- Assets/Game/Object/SwingPole/SwingAction.cs
+++ Assets/Game/Object/SwingPole/SwingAction.cs
@@ -9,10 +9,16 @@ public class SwingAction : Action
     public Collider swingTarget;
     [Header("Inputs")]
     public ActionInput swing;
+    public ActionInput horizontal;
+    public ActionInput vertical;
+
     [Header("Variables")]
     public float targetRange = 20;
     public float targetAngle = 70;
     public Vector2 swingSpeed;
+    [Space(10)]
+    public TargetAxis moveAxis;
+
     public float lerp;
 
     // Start is called before the first frame update
@@ -53,7 +59,6 @@ public class SwingAction : Action
 
         //Vector3 relVelocity = pole.InverseTransformDirection(Vector3.ProjectOnPlane(entity.body.velocity,upVector));
         fwdSpeed = 6 * Vector3.Dot(entity.body.velocity,fwdVector);//relVelocity.z;
-        Debug.Log(fwdSpeed);
 
         while(swingTarget != null && swing.GetInput() != 0)
         {
@@ -66,6 +71,12 @@ public class SwingAction : Action
             upVector = (swingPoint - transform.position).normalized;
             fwdVector = Vector3.Cross(upVector,pole.right);
             float normalAngle = Vector3.SignedAngle(Vector3.ProjectOnPlane(upVector,pole.right),Vector3.up,pole.right);
+
+            float hor = horizontal.GetInput();
+            float ver = vertical.GetInput();
+            Vector3 input =  moveAxis.forward * ver + moveAxis.right * hor;
+
+            fwdSpeed += Vector3.Dot(fwdVector,input) * swingSpeed.y * Time.fixedDeltaTime;
 
             fwdSpeed += normalAngle * 10 * Time.fixedDeltaTime;
             fwdSpeed = Mathf.SmoothDamp(fwdSpeed, 0, ref fwdRef, 1, 100,Time.fixedDeltaTime);
@@ -82,7 +93,7 @@ public class SwingAction : Action
         //entity.ChangeState("Idle");
         entity.actionLock = false;
         entity.body.useGravity = true;
-        entity.body.velocity = fwdVector.normalized * fwdSpeed/12;
+        entity.body.velocity = fwdVector.normalized * fwdSpeed/24;
     }
 
     Collider SwingCheck(float range, float maxAngle)
